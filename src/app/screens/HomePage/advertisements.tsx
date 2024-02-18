eb339d69db182d72ae34d46e18cce7cb101283a7
ortigaliev <ortigaliyevm@gmail.com>
2024-02-19 07:19:03 +0900

    feat: Publishing advertisements component for HomePage

--- src/app/screens/HomePage/advertisements.tsx
+++ src/app/screens/HomePage/advertisements.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@mui/material";
 import React from "react";
+import ads_video from "../../../../src/assets/ads_video.mp4";
 
 export function Advertisements() {
   return (
@@ -12,11 +13,7 @@ export function Advertisements() {
         playsInline
         data-video-media=""
       >
-        <source
-          data-src="https://fnb-p-c6e19b1675f4.imgix.net/87994cc2-0b2c-4b4e-a402-45c9834c267e/HP-6s-1920x1080-3_def.mp4?auto=compress%2Cformat&amp;fit=min&amp;fm=jpg&amp;q=80"
-          type="video/mp4"
-          src="https://fnb-p-c6e19b1675f4.imgix.net/87994cc2-0b2c-4b4e-a402-45c9834c267e/HP-6s-1920x1080-3_def.mp4?auto=compress%2Cformat&amp;fit=min&amp;fm=jpg&amp;q=80"
-        />
+        <source src={ads_video} />
       </video>
     </div>
   );
